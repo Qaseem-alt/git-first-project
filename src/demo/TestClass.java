6cd1ae7e89bb3ee08122d707de730b02642ce7f8
Qaseem-alt <qaseemj77@gmail.com>
2023-08-14 15:16:12 -0700

    Add another class

--- src/demo/TestClass.java
+++ src/demo/TestClass.java
@@ -2,4 +2,7 @@ package demo;
 
 public class TestClass {
 
+	public static void main(String[] args) {
+		System.out.println(" HI Testers");
+	}
 }
---
+++ src/demo/WhatIsUp.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class WhatIsUp {
+
+
+public static void main(String[] args) {
+	System.out.println("What is up");
+}
+	
+}
